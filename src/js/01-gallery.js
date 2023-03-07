f8cacca2a10049c98d066640b66a92bdc7e8f9ca
Oleksandr Kokhno <kohnoan@gmail.com>
2023-03-08 00:50:13 +0200

    commited01 08.03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const galleryEl = document.querySelector('.gallery');
+
+const makeGalleryCard =({
+    preview,
+    original,
+    description,
+}) => `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a></li>`;
+
+ const markupGallery = galleryItems.map((data) => makeGalleryCard(data)).join('');
+
+ galleryEl.insertAdjacentHTML('afterbegin', markupGallery);
+
+//  Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery.
+
+ let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+
+  galleryEl.setAttribute(
+    'style',
+    'list-style:none; padding: 30px;'
+  );
+
+  // justify-content: space-between; display: flex; gap: 30px;
