f099cfff597522dcec383064bac4491c5075202a
Oleksandr Kokhno <kohnoan@gmail.com>
2023-03-11 00:05:49 +0200

    commited02 10.03.2023

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+// player.on('play', function() {
+//   console.log('played the video!');
+// });
+
+player.getVideoTitle().then(function(title) {
+  console.log('title:', title);
+});
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+let seconds = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+if (seconds) {
+  player.setCurrentTime(seconds);
+}
