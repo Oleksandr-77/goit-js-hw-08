4d88c58f26e555845a61d1971f401ad8ea38f60f
Oleksandr Kokhno <kohnoan@gmail.com>
2023-03-12 21:10:50 +0200

    finished 03 12.03.23

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,59 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 const formEl = document.querySelector('form');
 const formInputEl = document.querySelector('input');
 const formTextareaEl = document.querySelector('textarea');
-const formSubmitBtnEl = document.querySelector('button');
-
-console.log(formEl);
-console.log(formInputEl);
-console.log(formTextareaEl);
-console.log(formSubmitBtnEl);
-console.log(localStorage);
+// const formSubmitBtnEl = document.querySelector('button');
 
 formEl.addEventListener('submit',onFormSubmit);
-formTextareaEl.addEventListener('input',throttle(onInput, 500));
+formEl.addEventListener('input',throttle(onInput, 500));
 
 getStorageItems();
 
 function onFormSubmit(event) {
   event.preventDefault();
 
-  console.log('Відправляємо форму');
+  if (event.target.elements.email.value && event.target.elements.message.value) {
+    console.log('Відправляємо форму');
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 
-  event.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {};
+  } else {
+    alert('заповни всі поля форми!');
+  }
 }
 
 function onInput(event) {
-  const message = event.target.value;
+  // const message = event.target.value;
 
   // console.log(message);
 
-  localStorage.setItem(STORAGE_KEY, message);
+  // localStorage.setItem(STORAGE_KEY, message);
+
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function getStorageItems() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  // const savedMessage = localStorage.getItem(STORAGE_KEY);
 
-  if (savedMessage) {
-    console.log(savedMessage);
-    formTextareaEl.value = savedMessage;
+  // if (savedMessage) {
+  //   console.log(savedMessage);
+  //   formTextareaEl.value = savedMessage;
+  // }
+
+  const storageItem = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (storageItem) {
+    console.log(storageItem);
+    formInputEl.value = storageItem.email || '';
+    formTextareaEl.value = storageItem.message || '';
+    console.log(storageItem);
   }
 }
